5c9d08c0342cfe0663e60cdfd2ded485fd8cf48a
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2023-01-02 20:50:01 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const wine = {
+    wine1: {
+        id: 1,
+        name: 'Kendall Jackson',
+        category: 'Chardonnay',
+        color: 'white',
+        vintage: '2020',
+        price: 21.99,
+        winery: 'Fulton, California, US'
+    },
+    wine2: {
+        id: 2,
+        name: 'Hardys',
+        category: 'Shiraz',
+        color: 'red',
+        vintage: '2020',
+        price: 10.99,
+        winery: 'Australia'
+    }
+}
+
+console.log(wine);
