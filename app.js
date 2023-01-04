adb798a6b919bd08891964ef885a77f30c987fd8
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2023-01-03 21:14:09 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-const wine = [
-    {
-        id: 1,
-        name: 'Kendall Jackson',
-        category: 'Chardonnay',
-        color: 'white',
-        vintage: '2020',
-        price: 21.99,
-        winery: 'Fulton, California, US'
-    },
-    {
-        id: 2,
-        name: 'Hardys',
-        category: 'Shiraz',
-        color: 'red',
-        vintage: '2020',
-        price: 10.99,
-        winery: 'Australia'
-    }
-]
+import wineList from './components/data.js';
 
-console.log(wine);
+const result = document.querySelector('#show-result');
 
-const wineList = document.querySelector('#wine-list');
-
-wine.forEach(item => {
+wineList.forEach(item => {
     const wineItem = document.createElement('ul');
     wineItem.innerHTML = `
     <li>${item.name}</li>
@@ -34,5 +13,5 @@ wine.forEach(item => {
     <li>${item.winery}</li>
     `
 
-    wineList.append(wineItem);
+    result.append(wineItem);
 });
---
+++ components/data.js
@@ -0,0 +1,22 @@
+const wineList = [
+    {
+        id: 1,
+        name: 'Kendall Jackson',
+        category: 'Chardonnay',
+        color: 'white',
+        vintage: '2020',
+        price: 21.99,
+        winery: 'Fulton, California, US'
+    },
+    {
+        id: 2,
+        name: 'Hardys',
+        category: 'Shiraz/Syrah',
+        color: 'red',
+        vintage: '2018',
+        price: 10.99,
+        winery: 'Hardys, South Eastern Australia'
+    }
+];
+
+export default wineList;
