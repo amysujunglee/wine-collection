a0bbac4240d123411906c958225591491a2b8171
Amy Lee <39070747+amysujunglee@users.noreply.github.com>
2023-01-05 20:58:30 -0800

    Add files via upload

--- components/data.js
+++ components/data.js
@@ -3,19 +3,28 @@ const wineList = [
         id: 1,
         name: 'Kendall Jackson',
         category: 'Chardonnay',
-        color: 'white',
+        color: 'White',
         vintage: '2020',
-        price: 21.99,
-        winery: 'Fulton, California, US'
+        price: 21.49,
+        winery: 'California, US'
     },
     {
         id: 2,
         name: 'Hardys',
         category: 'Shiraz/Syrah',
-        color: 'red',
-        vintage: '2018',
-        price: 10.99,
+        color: 'Red',
+        vintage: '',
+        price: 11.98,
         winery: 'Hardys, South Eastern Australia'
+    },
+    {
+        id: 3,
+        name: 'Kung Fu Girl',
+        category: 'Riesling',
+        color: 'White',
+        vintage: '2017',
+        price: 19.98,
+        winery: 'Washington, US'
     }
 ];
 
