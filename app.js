f7c06ee5b96c0bf9e9b4b382144650e2c11362e3
Amy Lee <amy.sujunglee@gmail.com>
2023-05-15 21:55:36 -0700

    filtering by grape

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ const result = document.querySelector('#show-result');
 const btnAll = document.querySelector('.btn.all')
 const btnRed = document.querySelector('.btn.red');
 const btnWhite = document.querySelector('.btn.white');
+const btnChardonnay = document.querySelector('.btn.chardonnay');
+const btnShiraz = document.querySelector('.btn.shiraz');
+const btnRiesling = document.querySelector('.btn.riesling');
+const btnNebbiolo = document.querySelector('.btn.nebbiolo');
 
 const showWineList = (wine) => {
     const wineItem = document.createElement('ul');
+    wineItem.className = 'wine-item';
     wineItem.innerHTML = `
     ${wine.image ? `<li><img src="${wine.image}" class="wine-img" /></li>` : ''}
     <li>${wine.name}</li>
-    <li>${wine.grape}</li>
+    <li>${wine.grape[0].toUpperCase() + wine.grape.slice(1)}</li>
     <li>${wine.type[0].toUpperCase() + wine.type.slice(1)}</li>
     <li>${wine.vintage}</li>
     <li>$${wine.price}</li>
@@ -57,6 +62,54 @@ btnWhite.addEventListener('click', () => {
     });
 
     whiteWine.forEach(wine => {
+        showWineList(wine);
+    });
+});
+
+btnChardonnay.addEventListener('click', () => {
+    resetList();
+
+    const chardonnayWine = wineList.filter(wine => {
+        return wine.grape === 'chardonnay';
+    });
+
+    chardonnayWine.forEach(wine => {
+        showWineList(wine);
+    });
+});
+
+btnShiraz.addEventListener('click', () => {
+    resetList();
+
+    const shirazWine = wineList.filter(wine => {
+        return wine.grape === 'shiraz';
+    });
+
+    shirazWine.forEach(wine => {
+        showWineList(wine);
+    });
+});
+
+btnRiesling.addEventListener('click', () => {
+    resetList();
+
+    const rieslingWine = wineList.filter(wine => {
+        return wine.grape === 'riesling';
+    });
+
+    rieslingWine.forEach(wine => {
+        showWineList(wine);
+    });
+});
+
+btnNebbiolo.addEventListener('click', () => {
+    resetList();
+
+    const nebbioloWine = wineList.filter(wine => {
+        return wine.grape === 'nebbiolo';
+    });
+
+    nebbioloWine.forEach(wine => {
         showWineList(wine);
     });
 });
